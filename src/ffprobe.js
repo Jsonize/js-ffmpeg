ecba06aa806b8c3678bfc0fbdff521fbfab7b370
Oliver Friedmann <oliver.friedmann@gmail.com>
2016-11-13 07:52:03 -0500

    Better error detection for ffprobe

--- src/ffprobe.js
+++ src/ffprobe.js
@@ -8,8 +8,16 @@ Scoped.require([
 			var promise = Promise.create();
 			var cmd = 'ffprobe -v quiet -print_format json -show_format -show_streams ' + file;
 			require("child_process").exec(cmd, function (error, stdout, stderr) {
-				var success = JSON.parse(stdout);
-				promise.asyncSuccess(success);
+				if (error) {
+					promise.asyncError("Cannot read file");
+					return;
+				}
+				try {
+					var success = JSON.parse(stdout);
+					promise.asyncSuccess(success);
+				} catch (e) {
+					promise.asyncError("Parse error: " + stdout);
+				}
 			});
 			return promise;
 		}
