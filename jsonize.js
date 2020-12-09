e4c5b8ba449b5086acc3829c34da788d4cc0f50b
Oliver Friedmann <oliver.friedmann@gmail.com>
2020-12-08 23:57:03 -0500

    Merge

---
+++ jsonize.js
@@ -0,0 +1,198 @@
+var jsffmpeg = require(__dirname + "/index.js");
+
+
+Scoped.define("jsonize:JsonizeFfprobeTask", [
+    "jsonize:AbstractJsonizeTask",
+    "jsonize:JsonizeTaskRegistry",
+    "betajs:Promise"
+], function (Class, TaskRegistry, Promise, scoped) {
+	var Cls = Class.extend({scoped: scoped}, {
+		
+		_run: function (payload) {
+			return jsffmpeg.ffprobe(payload.source, {
+				docker: payload.docker,
+                timeout: payload.timeout,
+				test_ffmpeg: payload.test_ffmpeg,
+				test_info: payload.test_info
+			});
+		}
+			
+	});
+	
+	TaskRegistry.register("ffprobe", Cls);
+	
+	return Cls;
+});
+
+
+Scoped.define("jsonize:JsonizeFfprobeSimpleTask", [
+     "jsonize:AbstractJsonizeTask",
+     "jsonize:JsonizeTaskRegistry",
+     "betajs:Promise"
+], function (Class, TaskRegistry, Promise, scoped) {
+ 	var Cls = Class.extend({scoped: scoped}, {
+ 		
+ 		_run: function (payload) {
+ 			return jsffmpeg.ffprobe_simple(payload.source, {
+                docker: payload.docker,
+                timeout: payload.timeout,
+                test_ffmpeg: payload.test_ffmpeg,
+                test_info: payload.test_info
+            });
+ 		}
+ 			
+ 	});
+ 	
+ 	TaskRegistry.register("ffprobe-simple", Cls);
+ 	
+ 	return Cls;
+});
+
+
+Scoped.define("jsonize:JsonizeFfmpegTask", [
+     "jsonize:AbstractJsonizeTask",
+     "jsonize:JsonizeTaskRegistry",
+     "betajs:Promise"
+], function (Class, TaskRegistry, Promise, scoped) {
+ 	var Cls = Class.extend({scoped: scoped}, {
+ 		
+ 		_run: function (payload) {
+ 			return jsffmpeg.ffmpeg(
+ 				payload.source || payload.sources,
+ 				payload.options || [],
+ 				payload.output,
+ 				this._event,
+ 				this,
+                {
+                    docker: payload.docker,
+                    timeout: payload.timeout,
+                    test_ffmpeg: payload.test_ffmpeg,
+                    test_info: payload.test_info
+                }
+ 			);
+ 		}
+ 			
+ 	});
+ 	
+ 	TaskRegistry.register("ffmpeg", Cls);
+ 	
+ 	return Cls;
+});
+
+
+Scoped.define("jsonize:JsonizeFfmpegMultiPassTask", [
+	  "jsonize:AbstractJsonizeTask",
+	  "jsonize:JsonizeTaskRegistry",
+	  "betajs:Promise"
+], function (Class, TaskRegistry, Promise, scoped) {
+  	var Cls = Class.extend({scoped: scoped}, {
+  		
+  		_run: function (payload) {
+  			return jsffmpeg.ffmpeg_multi_pass(
+  				payload.source || payload.sources,
+  				payload.options || [],
+  				2,
+  				payload.output,
+  				this._event,
+  				this,
+                {
+                    docker: payload.docker,
+                    timeout: payload.timeout,
+                    test_ffmpeg: payload.test_ffmpeg,
+                    test_info: payload.test_info
+                }
+  			);
+  		}
+  			
+  	});
+  	
+  	TaskRegistry.register("ffmpeg-multi-pass", Cls);
+  	
+  	return Cls;
+});
+
+
+Scoped.define("jsonize:JsonizeFfmpegSimpleTask", [
+    "jsonize:AbstractJsonizeTask",
+    "jsonize:JsonizeTaskRegistry",
+    "betajs:Promise"
+], function (Class, TaskRegistry, Promise, scoped) {
+	var Cls = Class.extend({scoped: scoped}, {
+		
+		_run: function (payload) {
+			return jsffmpeg.ffmpeg_simple(
+				payload.source || payload.sources,
+				payload.options || {},
+				payload.output,
+				this._event,
+				this,
+                {
+                    docker: payload.docker,
+                    timeout: payload.timeout,
+                    test_ffmpeg: payload.test_ffmpeg,
+                    test_info: payload.test_info
+                }
+			);
+		}
+			
+	});
+	
+	TaskRegistry.register("ffmpeg-simple", Cls);
+	
+	return Cls;
+});
+
+
+Scoped.define("jsonize:JsonizeFfmpegGracefulTask", [
+    "jsonize:AbstractJsonizeTask",
+    "jsonize:JsonizeTaskRegistry",
+    "betajs:Promise"
+], function (Class, TaskRegistry, Promise, scoped) {
+    var Cls = Class.extend({scoped: scoped}, {
+
+        _run: function (payload) {
+            return jsffmpeg.ffmpeg_graceful(
+                payload.source || payload.sources,
+                payload.options || {},
+                payload.output,
+                this._event,
+                this,
+                {
+                    docker: payload.docker,
+                    timeout: payload.timeout,
+                    test_ffmpeg: payload.test_ffmpeg,
+                    test_info: payload.test_info
+                }
+            );
+        }
+
+    });
+
+    TaskRegistry.register("ffmpeg-graceful", Cls);
+
+    return Cls;
+});
+
+
+Scoped.define("jsonize:JsonizeFfmpegVolumeDetectTask", [
+	"jsonize:AbstractJsonizeTask",
+	"jsonize:JsonizeTaskRegistry",
+	"betajs:Promise"
+], function (Class, TaskRegistry, Promise, scoped) {
+	var Cls = Class.extend({scoped: scoped}, {
+		
+		_run: function (payload) {
+			return jsffmpeg.ffmpeg_volume_detect(payload.source, {
+                docker: payload.docker,
+				timeout: payload.timeout,
+                test_ffmpeg: payload.test_ffmpeg,
+                test_info: payload.test_info
+            });
+		}
+			
+	});
+	
+	TaskRegistry.register("ffmpeg-volume-detect", Cls);
+	
+	return Cls;
+});
